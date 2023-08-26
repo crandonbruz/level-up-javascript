71e6edd62d6cfc8f73bb08feadb9c0efc9551f06
Brandon Cruz <123435219+crandonbruz@users.noreply.github.com>
2023-08-26 20:39:15 +0000

    completed first assignment

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,38 @@
-// Write your code here
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  get availability() {
+    return this.getAvailability();
+  }
+
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return "Out of stock";
+    } else if (this.numCopies < 10) {
+      return "Low stock";
+    }
+    return "In stock";
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+  
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+}
+
+const OnePiece = new Book("One Piece", "Eldin Ochiro", 11467, 100);
+console.log(OnePiece.availability);
+
+OnePiece.restock(8);
+console.log(OnePiece.availability);
+
+OnePiece.sell(24);
+console.log(OnePiece.availability);
